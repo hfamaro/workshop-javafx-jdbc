ff885a6745ae7ddf121f8870bec0d844b22cf4c4
Heliel Amaro <hfamaro@hotmail.com>
2021-01-15 12:13:49 -0400

    Validation exception

--- src/gui/DepartmentFormController.java
+++ src/gui/DepartmentFormController.java
@@ -3,7 +3,9 @@ package gui;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 import java.util.ResourceBundle;
+import java.util.Set;
 
 import db.DbException;
 import gui.listeners.DataChangeListener;
@@ -18,6 +20,7 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import model.entities.Department;
+import model.exceptions.ValidationException;
 import model.services.DepartmentService;
 
 public class DepartmentFormController implements Initializable {
@@ -53,7 +56,9 @@ public class DepartmentFormController implements Initializable {
 			Utils.currentStage(event).close();			
 		} catch (DbException e) {
 			Alerts.showAlert("Error saving object", null, e.getMessage(), AlertType.ERROR);
-		}		
+		} catch (ValidationException e) {
+			setErrorsMessages(e.getErrors());
+		}
 	}
 	
 	private void notifyDataChangeListeners() {
@@ -63,12 +68,34 @@ public class DepartmentFormController implements Initializable {
 		
 	}
 
-	private Department getFormData() {
+	private Department getFormData() {		
 		Department department = new Department();
+		
+		ValidationException exception = new ValidationException("Validation Error");
+		
 		department.setId(Utils.tryParseToInt(txtId.getText()));
+		
+		if (txtName.getText() == null || txtName.getText().trim().equals("")) {
+			exception.addError("name", "Field name can't be null");
+		}
+		
 		department.setName(txtName.getText());
+		
+		if (exception.getErrors().size() > 0) {
+			throw exception;
+		}
+		
 		return department;
 	}
+	
+	private void setErrorsMessages(Map<String, String> errors) {
+		Set<String> fields = errors.keySet();
+		
+		if (fields.contains("name")){
+			labelError.setText(errors.get("name"));			
+		}		
+	}
+	
 	@FXML
 	public void onBtCancelAction(ActionEvent event) {
 		Utils.currentStage(event).close();
