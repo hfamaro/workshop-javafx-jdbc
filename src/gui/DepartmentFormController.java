61ffdde7f81f3901f1020d281f7fad7f824836d5
Heliel Amaro <hfamaro@hotmail.com>
2021-01-15 11:46:36 -0400

    Observer design pattern to update tableview

--- src/gui/DepartmentFormController.java
+++ src/gui/DepartmentFormController.java
@@ -1,19 +1,22 @@
 package gui;
 
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.ResourceBundle;
 
 import db.DbException;
+import gui.listeners.DataChangeListener;
 import gui.util.Alerts;
 import gui.util.Constraints;
 import gui.util.Utils;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
-import javafx.scene.control.Alert.AlertType;
 import model.entities.Department;
 import model.services.DepartmentService;
 
@@ -21,6 +24,7 @@ public class DepartmentFormController implements Initializable {
 
 	private Department entity;
 	private DepartmentService service;
+	private List<DataChangeListener> dataChangeListeners = new ArrayList<>();
 	
 	@FXML
 	private TextField txtId;
@@ -45,14 +49,20 @@ public class DepartmentFormController implements Initializable {
 		try {
 			entity = getFormData();
 			service.saveOrUpdate(entity);
+			notifyDataChangeListeners();
+			Utils.currentStage(event).close();			
 		} catch (DbException e) {
 			Alerts.showAlert("Error saving object", null, e.getMessage(), AlertType.ERROR);
-		}
-		
-		Utils.currentStage(event).close();
-		
+		}		
 	}
 	
+	private void notifyDataChangeListeners() {
+		for (DataChangeListener dataChangeListener : dataChangeListeners) {
+			dataChangeListener.onDataChange();
+		}
+		
+	}
+
 	private Department getFormData() {
 		Department department = new Department();
 		department.setId(Utils.tryParseToInt(txtId.getText()));
@@ -89,6 +99,10 @@ public class DepartmentFormController implements Initializable {
 	private void initializeNodes() {
 		Constraints.setTextFieldInteger(txtId);
 		Constraints.setTextFieldMaxLength(txtName, 30);
+	}
+	
+	public void subscribeDataChangeListerns(DataChangeListener listener) {
+		dataChangeListeners.add(listener);
 	}
 
 }
